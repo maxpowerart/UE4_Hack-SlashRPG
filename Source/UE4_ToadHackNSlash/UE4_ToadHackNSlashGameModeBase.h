af64a4c9dc661af383106519a901509c6a89a10c
Arcadias-art <optimus20090@gmail.com>
2021-02-03 03:05:01 +0300

    init

---
+++ Source/UE4_ToadHackNSlash/UE4_ToadHackNSlashGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4_ToadHackNSlashGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_TOADHACKNSLASH_API AUE4_ToadHackNSlashGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
